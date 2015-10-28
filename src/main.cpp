e47ddbbcdbe5d1258f95491174f2befba6eeb026
Brandon Gutzmann <bgutz001@ucr.edu>
2015-10-28 12:15:26 -0700

    Added an input function. Gets input from the user.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
+#include <cstdio>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <stdlib.h>
 
 int execute(char* command[]);
+std::string input();
 
 int main() {
 	char* temp[] = {"ls", "-a", 0};
 	execute(temp);
 	char* temp2[] = {"exit", 0};
-	std::cout << "done" << std::endl;
 	execute(temp2);
-	std::cout << "done" << std::endl;
+
+	while (true) {
+		
+	}
     return 0;
 }
 
@@ -23,21 +27,28 @@ int execute(char* command[]) {
 	int status = 0;
 	if ((pid = fork()) < 0) {
 		std::cout << "Failed to create child proccess" << std::endl;
+		perror(0);
 		return -1;
 	}
 	if (pid == 0) { //child
 		if (execvp(command[0], command) == -1) {
 			std::cout << "Command failed" << std::endl;
+			perror(0);
 			exit(EXIT_FAILURE);
 		}
 		exit(EXIT_SUCCESS);
 	}
 	else { // parent
 		
-	//std::cout << "in parent" << pid << std::endl;
 			while(waitpid(pid, &status, 0) != pid);
 	
 		return 0;
 	}
 	return 0;
+}
+
+std::string input() {
+	std::string temp;
+	std::getline(std::cin, temp);
+	return temp;
 }
