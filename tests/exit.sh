c1b7a2296a38b3ccc7db728fbd57e9faf48cfb7d
Brandon Gutzmann <bgutz001@ucr.edu>
2015-11-06 18:09:42 -0800

    Finished exit.sh

--- tests/exit.sh
+++ tests/exit.sh
@@ -9,11 +9,27 @@ echo --------------------------------------------------------------------------
 
 echo "Testing Exit with || command"
 echo "ls || exit"
-echo "ls && exit"
+echo "dingle || exit"
 ../bin/rshell <<EOF
 ls || exit
+dingle || exit
+EOF
+echo "Success!"
+echo --------------------------------------------------------------------------
+
+echo "Testing Exit with && command"
+echo "dingle && exit"
+echo "ls && exit"
+../bin/rshell <<EOF
+dingle && exit
 ls && exit
 EOF
 echo "Success!"
 echo --------------------------------------------------------------------------
 
+echo "Testing Exit with ; command"
+echo "ls; exit"
+../bin/rshell <<EOF
+ls; exit
+EOF
+echo "Success!"
